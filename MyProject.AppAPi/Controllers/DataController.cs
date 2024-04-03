6b69ed57860e4ba9414bd6a27a87e6c5cbfc7ed4
mohammed-m-abdelhay <m.abdelhay.c@nhc.sa>
2024-04-03 12:03:01 +0300

    commit api

---
+++ MyProject.AppAPi/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyProject.Business;
+
+namespace MyProject.AppAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly Data _data;
+
+        public DataController(Data data)
+        {
+            _data = data;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_data.GetData());
+        }
+    }
+}
